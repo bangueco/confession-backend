ec5a47bf30ccdf62f168f062999df98b98322353
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-03 11:35:19 +0800

    Add required components

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const confessionsRouter = require('./controllers/confessions')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -17,7 +19,11 @@ mongoose.connect(config.MONGODB_URI)
   logger.error(`error connecting to MongoDB ${error.message}`)
 })
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
+
+app.use('/api/confessions', confessionsRouter)
+
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
