f15931ef5997eabb2581109e113aba1577e5299e
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-03 19:14:52 +0800

    Add error handling middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,6 +2,13 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'unknown endpoint'})
 }
 
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') return response.status(404).send({error: 'id not found'})
+
+  next(error)
+}
+
 module.exports = {
-  unknownEndpoint
+  unknownEndpoint,
+  errorHandler
 }
