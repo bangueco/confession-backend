d2e84a192ecb18653c02c667d79179d33d5fb489
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-06 09:35:17 +0800

    Update configuration

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 3001
+const MONGODB_URI = process.env.MONGODB_URI.replace('app_name', process.env.APP_NAME)
 
 module.exports = {
   PORT,
