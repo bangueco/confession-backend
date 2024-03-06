06e0c27a6f7aec28f540c155b77e7589dd5a1805
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-06 09:55:05 +0800

    Add environment log

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
+  logger.info(`Running on ${process.env.NODE_ENV} mode`)
   logger.info(`Server running on port ${config.PORT}`)
 })
